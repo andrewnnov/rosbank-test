cd6951cc054482e583f2b99033912db6f8e82073
andreybannikov <andrewnnov@yandex.ru>
2019-02-01 09:31:33 +0300

    add method define fio

--- morpher_test_task/src/main/java/ru/rosbank/morpher_test_task/controller/MorpherController.java
+++ morpher_test_task/src/main/java/ru/rosbank/morpher_test_task/controller/MorpherController.java
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.rosbank.spring_test.Declension;
+import ru.rosbank.spring_test.FIO;
+import ru.rosbank.spring_test.IFIO;
 import ru.rosbank.spring_test.Parse;
 import ru.rosbank.spring_test.utils.Case;
 import ru.rosbank.spring_test.utils.Gender;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/message")
@@ -16,24 +21,28 @@ public class MorpherController {
     @GetMapping("/gender")
     public Gender defineGender(String input) {
       Parse parse = new Parse(input);
-//      String gender = parse.getGender();
-//        System.out.println(gender);
-       return new Parse(input).getGender();
+      return new Parse(input).getGender();
     }
-//http://localhost:8080/message/2?input=Кошка&toGender=Masculine
+
     @GetMapping("/changeGender")
     public String exchangeGender(String input, Gender toGender) {
         Declension declension = new Declension();
         String newValue = declension.changeGender(input, toGender);
-//        System.out.println(newValue);
         return newValue;
     }
-//http://localhost:8080/message/3?formCase=Dat&isPlural=false
+
     @GetMapping("/definecase")
     public String definePadezh(Case formCase, boolean isPlural) {
         Parse parse = new Parse("Воздушный шар");
         String newValue = parse.getForm(formCase, isPlural);
         return newValue;
+    }
+
+    @GetMapping("/fio")
+    public FIO fio(String input) {
+        Parse parse = new Parse(input);
+        FIO name = (FIO) parse.getFIO();
+        return name;
     }
 
 
