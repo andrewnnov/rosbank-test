6353b213f0c13f7fb46d4cda7b944ec2cd337b07
andreybannikov <andrewnnov@yandex.ru>
2019-02-03 20:58:27 +0300

    change morph controller. delete path

--- morpher_test_task/src/main/java/ru/rosbank/morpher_test_task/controller/MorpherController.java
+++ morpher_test_task/src/main/java/ru/rosbank/morpher_test_task/controller/MorpherController.java
@@ -14,10 +14,6 @@ import ru.rosbank.spring_test.utils.Gender;
 public class MorpherController {
 
     private static final String DEFAULT_INPUT = "Воздушный шар";
-
-    //todo move to readme md
-    //http://localhost:8080/russian/message/gender?input=%D0%A1%D0%B5%D1%80%D0%B3%D0%B5%D0%B9
-
     /**
      * Этот метод определяет род вводимого слова
      * @param input Любое существительное или прилагательное
@@ -27,9 +23,6 @@ public class MorpherController {
     public Gender defineGender(String input) {
         return new Parse(input).getGender();
     }
-
-    //todo move to readme md
-    //http://localhost:8080/russian/message/changeGender?input=%D0%92%D0%BE%D0%B7%D0%B4%D1%83%D1%88%D0%BD%D1%8B%D0%B9&toGender=Feminine
 
     /**
      * Метод меняет род введеного слова мужского рода на женский или средний или переводит слово
@@ -46,9 +39,6 @@ public class MorpherController {
     public String exchangeGender(String input, Gender toGender) {
         return new Declension().changeGender(input, toGender);
     }
-
-    //todo move to readme md
-    //http://localhost:8080/russian/message/caseDefinition?formCase=Nom&isPlural=true
 
     /**
      * Метод для склонения словосочетания "воздушный шар"
@@ -67,9 +57,6 @@ public class MorpherController {
     public String defineCase(Case formCase, boolean isPlural) {
         return new Parse(DEFAULT_INPUT).getForm(formCase, isPlural);
     }
-
-    //todo move to readme md
-    //http://localhost:8080/russian/message/fio?input=%D0%9F%D1%83%D1%88%D0%BA%D0%B8%D0%BD%20%D0%90%D0%BB%D0%B5%D0%BA%D1%81%20%D0%A1%D0%B5%D1%80%D0%B3%D0%B5%D0%B5%D0%B2%D0%B8%D1%87
 
     /**
      * Метод выделяет фамилию, имя, отчество из введеной строки.
